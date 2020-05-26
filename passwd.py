28cadb99f0530c6f1de36209f2eac36446ce734b
eugene <filatov02@gmail.com>
2020-05-26 16:17:17 +0300

    started structuring program

--- passwd.py
+++ passwd.py
@@ -1,4 +1,50 @@
+from cryptography.fernet import Fernet
+import os
 
-def input_password():
-    print("inp pass")
-    return 0
+
+class PassHandler:
+
+    def get_password(self, vault):
+        key = self.get_key()
+        flag = self.check_password(vault, key)
+        return flag
+
+    @staticmethod
+    def create_key():
+        key = Fernet.generate_key()
+        with open("key.bin", "wb") as k:
+            k.write(key)
+
+    def add_password(self, filename):
+        new_pass = input("Enter new password: ")
+        with open(filename, "wb") as pas:
+            pas.write(bytes(new_pass))
+
+    @staticmethod
+    def encrypt_password():
+        pass
+
+    def get_key(self):
+        if not os.path.exists("key.bin") or os.stat("key.bin").st_size == 0:
+            self.create_key()
+            print("key created")
+        with open("key.bin", "rb") as k:
+            for line in k:
+                key = line
+        return key
+
+    @staticmethod
+    def check_password(vault, key):
+        pass_file = "pass%s.bin" % vault
+        with open(pass_file, "rb") as ps:
+            for line in ps:
+                enc_pass = line
+        decrypter = Fernet(key)
+        dec_pass = decrypter.decrypt(enc_pass)
+        inp_pass = input("Input your password: ")
+        if dec_pass == inp_pass:
+            print("success")
+            return 1
+        else:
+            print("incorrect password")
+            return 0
--- vault.py
+++ vault.py
@@ -1,20 +1,32 @@
-import passwd
+from passwd import PassHandler as Paswd
 import connect_sql
+import os
 
 
 class Vault:
 
     @staticmethod
-    def enter_vault():
+    def enter_vault(vault):
         print("Enter Vault")
+        key = Paswd.get_password(vault)
+        print(key)
 
     @staticmethod
-    def create_vault():
+    def create_vault(vault):
         print("create Vault")
+        if os.path.exists("vault%s.bin" % vault):
+            print("Err: vault exists")
+            return 0
+        pas = "vault%s.bin" % vault
+        Paswd.add_password(pas)
 
     @staticmethod
-    def delete_vault():
+    def delete_vault(vault):
         print("delete Vault")
+
+    @staticmethod
+    def err_handler(vault):
+        print("Error", vault)
 
     def start(self):
         pr = "Enter 1 to reach your vault\nEnter 2 to create new vault\n- "
@@ -24,8 +36,10 @@ class Vault:
             int(option)
         except ValueError:
             print("Error: input should be int")
-        action = options.get(option, lambda: "No such option")
-        action()
+            return 0
+        action = options.get(option, "err_handler")
+        vault = input("Enter vault name: ")
+        getattr(self, action)(vault)
 
 
 v1 = Vault()
