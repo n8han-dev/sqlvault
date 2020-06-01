3571c8385d2d99b9b6f6617e90ce6e6398d0f280
n8han-dev <63315316+n8han-dev@users.noreply.github.com>
2020-06-01 20:47:07 +0300

    Test1 (#1)
    * testing no/conflict git functions
    
    * test git merge
    
    * commit branch test1
    
    * commit branch test2
    
    * added 3 test files to test1 branch
    
    * commiting to test2

--- connect_sql.py
+++ connect_sql.py
@@ -1,3 +1,14 @@
+import mysql.connector
 
-def connect():
-    print("connected")
+
+class Sql:
+
+    def connect(self):
+        print("connect")
+
+    def conflict(self):
+        print("conflict line 1")
+        print("conflict line 2")
+
+    def no_conflict(self):
+        pass  # i don't conflict
--- passwd.py
+++ passwd.py
@@ -2,49 +2,59 @@ from cryptography.fernet import Fernet
 import os
 
 
-class PassHandler:
+class Password:
 
     def get_password(self, vault):
-        key = self.get_key()
-        flag = self.check_password(vault, key)
+        key_file = "key%s.bin" % vault
+        vault_file = "vault%s.bin" % vault
+        key = self.get_key(key_file)
+        if not key:
+            return 0
+        flag = self.check_password(vault_file, key)
         return flag
 
     @staticmethod
-    def create_key():
+    def create_key(key_name):
         key = Fernet.generate_key()
-        with open("key.bin", "wb") as k:
+        with open(key_name, "wb") as k:
             k.write(key)
 
-    def add_password(self, filename):
+    def add_password(self, vault):
         new_pass = input("Enter new password: ")
-        with open(filename, "wb") as pas:
-            pas.write(bytes(new_pass))
+        pass_file = "vault%s.bin" % vault
+        key_name = "key%s.bin" % vault
+        self.create_key(key_name)
+        with open(key_name, "rb") as k:
+            for line in k:
+                key = line
+        encrypts = Fernet(key)
+        enc_password = encrypts.encrypt(bytes(new_pass, "utf-8"))
+        with open(pass_file, "wb") as pas:
+            pas.write(enc_password)
+        print("Created key and password for vault%s" % vault)
 
     @staticmethod
-    def encrypt_password():
-        pass
-
-    def get_key(self):
-        if not os.path.exists("key.bin") or os.stat("key.bin").st_size == 0:
-            self.create_key()
-            print("key created")
-        with open("key.bin", "rb") as k:
+    def get_key(file):
+        if not os.path.exists(file) or os.stat(file).st_size == 0:
+            print("Error: no key found")
+            return False
+        with open(file, "rb") as k:
             for line in k:
                 key = line
         return key
 
     @staticmethod
-    def check_password(vault, key):
-        pass_file = "pass%s.bin" % vault
+    def check_password(pass_file, key):
         with open(pass_file, "rb") as ps:
             for line in ps:
                 enc_pass = line
         decrypter = Fernet(key)
-        dec_pass = decrypter.decrypt(enc_pass)
+        dec_pass = decrypter.decrypt(enc_pass).decode("utf-8")
         inp_pass = input("Input your password: ")
+        print(dec_pass, inp_pass)
         if dec_pass == inp_pass:
             print("success")
-            return 1
+            return True
         else:
             print("incorrect password")
-            return 0
+            return False
